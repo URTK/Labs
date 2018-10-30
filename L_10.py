06fa59d2f25cea179e8ff4fa2e3d9839b3e6f4e0
УРТК <43788393+URTK@users.noreply.github.com>
2018-10-30 14:42:22 +0500

    Update L_10.py

--- L_10.py
+++ L_10.py
@@ -1,53 +1,17 @@
-packofCards = ['Трефы', 'Черви', 'Черви', 'Черви']
-
-# Если нужно найти последовательность из 3х карт любой масти.
-
-def shuffle(pack):
+def shuffle(suit):
     counter = 0
-    for i in range(1, len(pack)):
-        if pack[i] == pack[i - 1]:
+    value = suit[0]
+    for item in suit:
+        if item == value:
             counter += 1
-            if counter == 2:
-                return pack[i]
         else:
             counter = 0
-    return 'Нет трех идущих подряд карт одной масти'
-
-print(shuffle(packofCards))
-
-
-
-# Если нужно найти последовательность карт  определенной масти.
-
-sought_val = 'Черви'
-
-
-def shuffle(pack, val):
-    counter = 0
-    for i in range(len(pack)):
-        if pack[i] == val:
-            counter += 1
-            if counter == 3:
-                return True
-        else:
-            counter = 0
+            value = item
+        if counter >= 3:
+            return True
     return False
 
 
-print(shuffle(packofCards, sought_val))
+packOfCards = ['Черви','Черви','Черви']
 
-packofCards = ['Черви','Черви','Черви']
-value = packofCards[0]
-
-def shuffle(packofCards):
-    counter = 0
-    for index, item in enumerate(packofCards):
-        if item == value:
-            value = item
-            counter += 1
-            if counter >=3:
-                return 0
-            else:
-                return 'Нет трех идущих подряд карт одной масти'
-
-print(shuffle(packofCards))
+print(shuffle(packOfCards))
